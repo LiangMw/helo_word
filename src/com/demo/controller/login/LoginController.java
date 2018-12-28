a661fd7f2a16c28a0a81186f0be15e4e8220e7e6
liangmingwei <85713534@qq.com>
2018-12-28 10:08:59 +0800

    修改

--- src/com/demo/controller/login/LoginController.java
+++ src/com/demo/controller/login/LoginController.java
@@ -3,66 +3,67 @@ package com.demo.controller.login;
 import com.demo.common.model.SToken;
 import com.demo.common.model.SUser;
 import com.demo.controller.ControllerExt;
-import com.demo.interceptor.UserLoginIntercepter;
+import com.demo.interceptor.AutherIntercepter;
 import com.demo.util.MatchUtils;
 import com.jfinal.aop.Before;
 import com.jfinal.aop.Clear;
 import com.jfinal.kit.StrKit;
 
 public class LoginController extends ControllerExt {
-	
+
 	private LoginService ls = new LoginService();
-	
+
 	@Clear
-	public void register(){
+	public void register() {
 		String tel = getPara("tel");
 		String pwd = getPara("pwd");
-		if(StrKit.isBlank(tel)||StrKit.isBlank(pwd)){
+		if (StrKit.isBlank(tel) || StrKit.isBlank(pwd)) {
 			renderAppError("账号或密码不能为空");
-		}else{
+		} else {
 			String telmatch = MatchUtils.isPhone(tel);
 			String pwdmatch = MatchUtils.isPwdSequarty(pwd);
-			if("1".equals(telmatch)){
-				if("1".equals(pwdmatch)){
-					if(ls.register(tel, pwd)){
+			if ("1".equals(telmatch)) {
+				if ("1".equals(pwdmatch)) {
+					if (ls.register(tel, pwd)) {
 						renderAppSuccess("成功");
-					}else{
+					} else {
 						renderAppError("失败");
 					}
-				}else{
+				} else {
 					renderAppError(pwdmatch);
 				}
-			}else{
+			} else {
 				renderAppError(telmatch);
 			}
 		}
 	}
-	
+
 	@Clear
-	public void login(){
+	public void login() {
 		String tel = getPara("tel");
 		String pwd = getPara("pwd");
-		if(StrKit.isBlank(tel)||StrKit.isBlank(pwd)){
+		if (StrKit.isBlank(tel) || StrKit.isBlank(pwd)) {
 			renderAppError("账号或密码不能为空");
-		}else{
+		} else {
 			SToken token = ls.login(tel, pwd);
-			if(token == null){
+			if (token == null) {
 				renderAppError("账号或密码错误");
-			}else{
+			} else {
 				renderAppJson(token);
 			}
 		}
 	}
-	
+
 	@Clear
-	@Before(UserLoginIntercepter.class)
-	public void userinfo(){
+	@Before(AutherIntercepter.class)
+	public void userinfo() {
 		String guid = getPara("guid");
-			SUser u = ls.userInfo(guid);
-			if(u == null){
-				renderAppError("用户名错误");
-			}else{
-				renderAppJson(u);
-			}
+		SUser u = ls.userInfo(guid);
+		if (u == null) {
+			renderAppError("无此用户");
+		} else {
+			renderAppJson(u);
 		}
+	}
+	
 }
--- src/com/demo/interceptor/AutherIntercepter.java
+++ src/com/demo/interceptor/AutherIntercepter.java
@@ -4,12 +4,11 @@ import com.demo.common.model.SToken;
 import com.demo.controller.ControllerExt;
 import com.demo.util.SysStatus;
 import com.jfinal.aop.Interceptor;
-import com.jfinal.aop.InterceptorManager;
 import com.jfinal.aop.Invocation;
+import com.jfinal.kit.HashKit;
 import com.jfinal.kit.StrKit;
-import com.jfinal.plugin.activerecord.Db;
 
-public class UserLoginIntercepter implements Interceptor{
+public class AutherIntercepter implements Interceptor{
 	
 	public static String token = "";
 
@@ -17,15 +16,48 @@ public class UserLoginIntercepter implements Interceptor{
 	public void intercept(Invocation inv) {
 		// TODO Auto-generated method stub
 		String guid = inv.getController().getPara("guid");
+		String sign = inv.getController().getPara("sign");
 		if(StrKit.isBlank(token)){
 			token = getToken(guid);
 		}
+		System.out.println( token);
 		System.out.println( inv.getControllerKey() + "/" + inv.getMethodName() + " ,  " + inv);
+		System.out.println( inv.getActionKey());
+		String key = inv.getControllerKey() + "/" + inv.getMethodName();
 		if (token.equals(SysStatus.CODETOKENERROR)) {
 			((ControllerExt) inv.getController()).renderAppErrorAndCode("Token错误，请重新登录！", SysStatus.CODETOKENERROR);
 			return;
 		}
+		System.out.println( inv.getActionKey());
+		if(!checkToken(sign,inv.getActionKey(),token)){
+			((ControllerExt) inv.getController()).renderAppErrorAndCode("认证信息出错，请重新登录！", SysStatus.CODETOKENERROR);
+			return;
+		}
 		inv.invoke();
+	}
+	
+	/**
+	 * 根据请求中加密后的sign信息验证请求中的token是否合法 sign为md5(url+token)的签名。eg:md5(/user/info?token=qfSMacpcaTJZPrkk)
+	 * 
+	 * @param c
+	 * @param cKey
+	 * @return
+	 */
+	public static boolean checkToken(String sign, String cKey,String token) {
+		//String uid = c.getPara("uid");
+		if (StrKit.isBlank(sign)) {
+			return false;
+		}
+		if (token != null) {
+			String newsign;
+			newsign = cKey + "?token=" + token;
+			System.out.println("----"+HashKit.md5(newsign));
+
+			if (sign.equals(HashKit.md5(newsign))) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	/**
